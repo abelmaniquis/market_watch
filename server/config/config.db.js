59d373c8b4fb423a0a3cdfb7b95b233eae409ccd
abelmaniquis <abelmaniquis@gmail.com>
2016-11-27 16:00:33 +0000

    using production database

--- server/config/config.db.js
+++ server/config/config.db.js
@@ -3,7 +3,9 @@ module.exports = {
   seed:true,
   db:
   {
-    //url:'mongodb://localhost/marketUsers'
     url:process.env.MONGOLAB_URI
   }
 }
+
+/*development 
+url:'mongodb://localhost/marketUsers'*/
